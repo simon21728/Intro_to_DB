ae6664b87e5691601a8d60be8d31b777c41af92b
sewmehon bayu <simonbayu2211@gmail.com>
2025-01-31 01:49:18 -0900

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,29 +5,36 @@ def create_database():
     try:
         # Establish the connection to MySQL server
         connection = mysql.connector.connect(
-            host='127.0.0.1',  # Update with your host, typically 'localhost'
+            host='localhost',  # Update with your host, typically 'localhost'
             user='root',       # Update with your MySQL username
-            password='Sew76@bayu21' # Update with your MySQL password
+            password='password' # Update with your MySQL password
         )
-        
+
         if connection.is_connected():
             # Create a cursor object to interact with MySQL server
             cursor = connection.cursor()
-            
+
             # Create database if it doesn't already exist
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-            
+
             # Close the cursor and connection after the operation is complete
             cursor.close()
             connection.close()
 
-    except Error as e:
-        # Handle any errors that occur during the connection or execution
-        print(f"Error: {e}")
+    except mysql.connector.Error as err:
+        # Handle MySQL-specific errors
+        print(f"MySQL Error: {err}")
+        if connection.is_connected():
+            connection.close()
+
+    except Exception as err:
+        # Handle any other general errors
+        print(f"Error: {err}")
         if connection.is_connected():
             connection.close()
 
 if __name__ == "__main__":
     create_database()
+
 
