f32c261e2639aefd29a87e1a6a1d2084ad707143
sewmehon bayu <simonbayu2211@gmail.com>
2025-01-31 01:44:52 -0900

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,9 +5,9 @@ def create_database():
     try:
         # Establish the connection to MySQL server
         connection = mysql.connector.connect(
-            host='localhost',  # Update with your host, typically 'localhost'
+            host='127.0.0.1',  # Update with your host, typically 'localhost'
             user='root',       # Update with your MySQL username
-            password='password' # Update with your MySQL password
+            password='Sew76@bayu21' # Update with your MySQL password
         )
         
         if connection.is_connected():
