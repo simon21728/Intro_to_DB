b74b4a12039d0e96abbe6836b2ebe84bf989578c
sewmehon bayu <simonbayu2211@gmail.com>
2025-01-31 03:42:43 -0900

    Populating Your Database with Your Very First Data

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Insert a single row into the customers table with the given values
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
