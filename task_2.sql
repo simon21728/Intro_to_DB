2e841da626c57fceaff9d274b6b8bee2e6bbda50
sewmehon bayu <simonbayu2211@gmail.com>
2025-01-31 04:57:22 -0900

    Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 );
 
 -- Create the books table
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS books (
 );
 
 -- Create the customers table
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS customers (
 );
 
 -- Create the orders table
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS orders (
 );
 
 -- Create the order_details table
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
